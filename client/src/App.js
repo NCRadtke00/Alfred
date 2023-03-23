954996942347aa8c519664ae25090e3317778a33
Nicholas Radtke <raddnc@gmail.com>
2023-03-23 01:00:04 -0500

    added client side correctly to repo, also set up rough styling

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import "./normal.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <aside className='side-menu'>
+        <h1>Side Menu</h1>
+      </aside>
+      <section className="message-log">
+        <h1>Message Log</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
