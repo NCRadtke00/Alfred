4a5fbf9cb32b8dd8527d201d2b33b65a68b5c655
Nicholas Radtke <raddnc@gmail.com>
2023-03-23 12:38:00 -0500

    sever side set, client side roughly set up, no storage is set up yet. will be rebuilding application as alfred0.0.1. will begin testing api calls for responses

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
 const configuration = new Configuration({
 
-});
+})
 const openai = new OpenAIApi(configuration);
-const app = express();
+//const response = await openai.listEngines();
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 const port = 3080
-app.post('/', async (req, res) => {
+
+app.post("/", async (req, res) => {
+    const { message } = req.body;
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
     });
-    console.log(response.data.choices[0].text)
     res.json({
-        data: response.data
-    });
-});
+        message: response.data.choices[0].text,
+    })
+})
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+    console.log("Example app listening at http://localhost:$(port)")
+})
